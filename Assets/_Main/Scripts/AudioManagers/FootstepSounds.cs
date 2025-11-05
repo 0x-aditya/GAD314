0950e7ad25ecd41a2595e96a19b33a3f371ad5e5
NullPerson <edu1khoory@hotmail.com>
2025-11-05 11:44:44 +0400

    Audio Implementation finalizing

--- Assets/_Main/Scripts/AudioManagers/FootstepSounds.cs
+++ Assets/_Main/Scripts/AudioManagers/FootstepSounds.cs
@@ -12,20 +12,37 @@ public class FootstepSounds : MonoBehaviour
 
     [SerializeField] private AudioClip[] footsteps;
 
+    [SerializeField] private float delay;
+    private float maxDelay;
+
+    private Rigidbody2D _testSpeed;
+
     [SerializeField] private GameObject player;
 
     void Start()
     {
-        
+        maxDelay = delay;
+        _testSpeed = player.GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
+        if (player == null)
+        {
+            return;
+        }
+
+        delay -= Time.deltaTime;
+
         if (footsteps != null)
         {
-            _playerSound.volume = UnityEngine.Random.Range(0, footsteps.Length);
-            _playerSound.pitch = UnityEngine.Random.Range(0.8f, 1.15f);
-            _playerSound.PlayOneShot(footsteps[UnityEngine.Random.Range(0, footsteps.Length)]);
+            if (_testSpeed.linearVelocity.magnitude > 0f && delay <= 0)
+            {
+                delay = maxDelay;
+                //_playerSound.volume = UnityEngine.Random.Range(0, footsteps.Length);
+                _playerSound.pitch = UnityEngine.Random.Range(0.8f, 1.15f);
+                _playerSound.PlayOneShot(footsteps[UnityEngine.Random.Range(0, footsteps.Length)]);
+            }
         }
     }
 }
