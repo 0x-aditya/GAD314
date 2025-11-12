b8bcd56e52d94805001ebd8c9e58ffff51b7bf1c
NullPerson <edu1khoory@hotmail.com>
2025-11-12 11:57:29 +0400

    Completed Footstep System

--- Assets/_Main/Scripts/AudioManagers/FootstepSounds.cs
+++ Assets/_Main/Scripts/AudioManagers/FootstepSounds.cs
@@ -8,11 +8,20 @@ using Unity.Mathematics;
 public class FootstepSounds : MonoBehaviour
 {
 
+    
+
     [SerializeField] private AudioSource _playerSound;
 
-    [SerializeField] private AudioClip[] footsteps;
+    [SerializeField] private AudioClip[] footstepsGrass;
+    [SerializeField] private AudioClip[] footstepsStone;
+    [SerializeField] private AudioClip[] footstepsDirt;
+
+    [SerializeField] private string surfaceName;
 
     [SerializeField] private float delay;
+
+    [SerializeField] private LayerMask _layerMask;
+
     private float maxDelay;
 
     private Rigidbody2D _testSpeed;
@@ -32,17 +41,54 @@ public class FootstepSounds : MonoBehaviour
             return;
         }
 
+        
+
         delay -= Time.deltaTime;
 
-        if (footsteps != null)
-        {
+
             if (_testSpeed.linearVelocity.magnitude > 0f && delay <= 0)
             {
                 delay = maxDelay;
-                //_playerSound.volume = UnityEngine.Random.Range(0, footsteps.Length);
                 _playerSound.pitch = UnityEngine.Random.Range(0.8f, 1.15f);
-                _playerSound.PlayOneShot(footsteps[UnityEngine.Random.Range(0, footsteps.Length)]);
-            }
+                GetSurfaceType();
+
+            switch(surfaceName)
+            {
+
+                case "Grass":
+                _playerSound.PlayOneShot(footstepsGrass[UnityEngine.Random.Range(0, footstepsGrass.Length)]);
+                    break;
+                case "Dirt":
+                    _playerSound.PlayOneShot(footstepsDirt[UnityEngine.Random.Range(0, footstepsDirt.Length)]);
+                    break;
+                case "Stone":
+                    _playerSound.PlayOneShot(footstepsStone[UnityEngine.Random.Range(0, footstepsStone.Length)]);
+                    break;
+                default:
+                    break;
         }
+
+                 
+            }
     }
+
+    private void GetSurfaceType()
+    {
+        RaycastHit2D hit;
+
+        hit = Physics2D.Raycast(_playerSound.transform.position, Vector2.down, 0.1f, _layerMask);
+
+        if (hit.collider != null)
+        {
+            string newSurfaceTag = hit.collider.tag;
+
+            if (newSurfaceTag != surfaceName)
+            {
+                surfaceName = newSurfaceTag;
+            }
+
+        }
+        
+    }
+
 }
